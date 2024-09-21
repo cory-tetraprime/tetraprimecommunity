69e1a525c6ce9cbbb182a5cb6e32e425169a3daa
Cory Via <cory@tetraprimeconsulting.com>
2024-09-21 09:32:06 -0400

    First commit.

---
+++ tetraprimecommunity/tetraprimecommunity/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-=xz4^sc3rtcj7)hnq=f^n-(7d5#qlnq4*veqgo-6cm45+#047a"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
